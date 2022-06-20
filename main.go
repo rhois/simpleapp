68eed9e9a65f3dcee421481d871ccecb4eb2ecf2
Rois <rois.alq@gmail.com>
2022-06-20 17:52:17 +0700

    update callback result

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func callback(w http.ResponseWriter, req *http.Request) {
+	var p map[string]string
+	err := json.NewDecoder(req.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(p["link"])
+}
+
+func main() {
+
+	http.HandleFunc("/callback", callback)
+
+	http.ListenAndServe(":8090", nil)
+}
